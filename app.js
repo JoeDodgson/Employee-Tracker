036d1eeceec39a72436faa0eeceb02cd68b392d8
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-16 18:29:30 +0100

    Created connection to SQL database

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const mysql = require("mysql");
 const questions = require("./questions");
 const ChoiceQuestion = questions.ChoiceQuestion;
 const PromptQuestion = questions.PromptQuestion;
+
+// Create connection to the SQL server
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employeetracker"
+})

6013fb58355cd8eba82444b525ddf5f1bbfd7296
Joe Dodgson <Joe.Dodgson@arup.com>
2020-06-17 15:23:01 +0100

    Queried the database to return a list of all employees

--- app.js
+++ app.js
@@ -220,7 +220,9 @@ async function addEmployee() {
 async function removeEmployee() {
     try {
         // Query the database to return a list of employees
-       
+        const employeesListData = await queryAsync("SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee;");
+        const employeesList = employeesListData.map(employee => employee.name);
+
         // Generate a question using the returned employees    
 
         // Prompts user to select an employee
